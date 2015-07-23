5bbf20a3a695632e391497f55984440c28b2ab4b
TC500 <xwm522@126.com>
2015-07-23 17:52:02 +0800

    简介页面完善，添加了按钮响应和展开/折叠  by马霄伟

---
+++ src/com/qihoo/feiyang/ui/simple_introduce_test_activity_mxw.java
@@ -0,0 +1,221 @@
+package com.qihoo.feiyang.ui;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import com.qihoo.feiyang.challenger.R;
+
+/**
+ * Created by maxiaowei on 2015/7/23.
+ * 简介(simple_introduce)页面的activity
+ */
+public class simple_introduce_test_activity_mxw extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maxw_simple_introduce_layout);
+
+        /*提醒详情的展开按钮控制---------------------------------------------*/
+        //展开文字textView
+        TextView remain_expend_textView = (TextView)findViewById(R.id.simple_introduce_remain_expend);
+        //textView单击响应
+        remain_expend_textView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                //提醒详情布局句柄
+                View remind_details = (View) findViewById(R.id.simple_introduce_remind_details_RLayout);
+                //箭头图片ImageView
+                ImageView remain_expend_imageView = (ImageView) findViewById(R.id.simple_introduce_remain_expend_imageView);
+                //页面处于展开状态
+                if (remind_details.getVisibility() == View.VISIBLE) {
+                    //收起
+                    remind_details.setVisibility(View.GONE);
+                    //修改textView文字
+                    remain_expend_textView.setText(R.string.expend);
+                    //修改箭头图标
+                    remain_expend_imageView.setImageResource(R.drawable.arrow_downlad);
+                    Log.v("debug", "unexpend");
+                }
+                //页面处于收起状态
+                else {
+                    //展开
+                    remind_details.setVisibility(View.VISIBLE);
+                    //修改textView文字
+                    remain_expend_textView.setText(R.string.unexpend);
+                    //修改箭头图标
+                    remain_expend_imageView.setImageResource(R.drawable.arrow_up);
+                    Log.v("debug", "expend");
+                }
+            }
+        });
+        /*--------------------------------------------------------------------------*/
+
+        /*应用描述的展开按钮控制---------------------------------------------*/
+        //展开文字textView
+        TextView app_introduce_expend_textView = (TextView)findViewById(R.id.simple_introduce_app_introduce_expend);
+        //textView单击响应
+        app_introduce_expend_textView.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                // TODO Auto-generated method stub
+                //应用介绍详情布局句柄
+                View app_introduce_details = (View)findViewById(R.id.simple_introduce_app_introduce_details);
+                //箭头图片ImageView
+                ImageView app_introduce_expend_imageView = (ImageView)findViewById(R.id.simple_introduce_app_introduce_expend_imageView);
+                //页面处于展开状态
+                if(app_introduce_details.getVisibility() == View.VISIBLE)
+                {
+                    //收起
+                    app_introduce_details.setVisibility(View.GONE);
+                    //修改textView文字
+                    app_introduce_expend_textView.setText(R.string.expend);
+                    //修改箭头图标
+                    app_introduce_expend_imageView.setImageResource(R.drawable.arrow_downlad);
+                    Log.v("debug", "unexpend");
+                }
+                //页面处于收起状态
+                else
+                {
+                    //展开
+                    app_introduce_details.setVisibility(View.VISIBLE);
+                    //修改textView文字
+                    app_introduce_expend_textView.setText(R.string.unexpend);
+                    //修改箭头图标
+                    app_introduce_expend_imageView.setImageResource(R.drawable.arrow_up);
+                    Log.v("debug", "expend");
+                }
+            }
+        });
+        /*----------------------------------------------------------------------------*/
+
+        /*举报按钮控制---------------------------------------------------------------*/
+        //举报按钮
+        Button complain_button = (Button)findViewById(R.id.simple_introduce_complain_button);
+        //textView单击响应
+        complain_button.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                // TODO Auto-generated method stub
+                //应用介绍详情布局句柄
+                Log.v("debug", "comlain_button");
+                showString("点击了举报按钮");
+            }
+        });
+        /*----------------------------------------------------------------------------*/
+
+        /*查看更多按钮控制---------------------------------------------------------------*/
+        //查看更多textView
+        TextView other_like_more_button = (TextView)findViewById(R.id.simple_introduce_other_like_more);
+        //textView单击响应
+        other_like_more_button.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                // TODO Auto-generated method stub
+                //应用介绍详情布局句柄
+                Log.v("debug", "other_like_more");
+                showString("查看更多按钮");
+            }
+        });
+        /*----------------------------------------------------------------------------*/
+
+        /*游戏1下载按钮控制---------------------------------------------------------------*/
+        //查看更多textView
+        Button other_like_game1_button = (Button)findViewById(R.id.simple_introduce_other_like_game1_button);
+        //textView单击响应
+        other_like_game1_button.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                // TODO Auto-generated method stub
+                //应用介绍详情布局句柄
+                Log.v("debug", "game1_download");
+                showString("游戏1下载按钮");
+            }
+        });
+        /*----------------------------------------------------------------------------*/
+
+
+        /*游戏1下载按钮控制---------------------------------------------------------------*/
+        //查看更多textView
+        Button other_like_game2_button = (Button)findViewById(R.id.simple_introduce_other_like_game2_button);
+        //textView单击响应
+        other_like_game2_button.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                // TODO Auto-generated method stub
+                //应用介绍详情布局句柄
+                Log.v("debug", "game2_download");
+                showString("游戏2下载按钮");
+            }
+        });
+        /*----------------------------------------------------------------------------*/
+
+
+        /*游戏1下载按钮控制---------------------------------------------------------------*/
+        //“查看更多”textView
+        Button other_like_game3_button = (Button)findViewById(R.id.simple_introduce_other_like_game3_button);
+        //textView单击响应
+        other_like_game3_button.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                // TODO Auto-generated method stub
+                //应用介绍详情布局句柄
+                Log.v("debug", "game3_download");
+                showString("游戏3下载按钮");
+            }
+        });
+        /*----------------------------------------------------------------------------*/
+
+
+        /*游戏礼包框架点击控制---------------------------------------------------------------*/
+        //游戏礼包框架View
+        View gift_layout = (View)findViewById(R.id.simple_introduce_gift_layout);
+        //textView单击响应
+        gift_layout.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                // TODO Auto-generated method stub
+                //应用介绍详情布局句柄
+                Log.v("debug", "gift_layout");
+                showString("点击了游戏礼包布局");
+            }
+        });
+        /*----------------------------------------------------------------------------*/
+
+
+
+    }
+
+    private void showString(String input)
+    {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setMessage(input).create();
+        Window window = dialog.getWindow();
+        window.setGravity(Gravity.BOTTOM);  //此处可以设置dialog显示的位置
+        dialog.show();
+    }
+}
